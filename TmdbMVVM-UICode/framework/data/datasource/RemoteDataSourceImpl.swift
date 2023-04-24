01784b5319845fa821326b846c49e5c939b50f0d
Dhipa Poltak <dhipa2poltak@gmail.com>
2023-04-24 21:38:46 +0700

    implementing "Framework" layer

---
+++ TmdbMVVM-UICode/framework/data/datasource/RemoteDataSourceImpl.swift
@@ -0,0 +1,38 @@
+//
+//  RemoteDataSourceImpl.swift
+//  TmdbMVVM-UICode
+//
+//  Created by user on 24/04/23.
+//
+
+import Foundation
+import RxSwift
+
+class RemoteDataSourceImpl: AppDataSource {
+
+    let apiClient: ApiClient
+
+    init(apiClient: ApiClient) {
+        self.apiClient = apiClient
+    }
+
+    func fetchMovieGenre() -> Observable<GenreResponse> {
+        return apiClient.fetchMovieGenre()
+    }
+
+    func fetchMovieByGenre(genreId: String, page: Int) -> Observable<DiscoverMovieByGenreResponse> {
+        return apiClient.fetchMovieByGenre(genreId: genreId, page: page)
+    }
+
+    func fetchMovieDetail(movieId: Int) -> Observable<MovieDetailsResponse> {
+        return apiClient.fetchMovieDetail(movieId: movieId)
+    }
+
+    func fetchMovieReviews(movieId: Int, page: Int) -> Observable<ReviewResponse> {
+        return apiClient.fetchMovieReviews(movieId: movieId, page: page)
+    }
+
+    func fetchMovieTrailer(movieId: Int) -> Observable<TrailerResponse> {
+        return apiClient.fetchMovieTrailer(movieId: movieId)
+    }
+}
