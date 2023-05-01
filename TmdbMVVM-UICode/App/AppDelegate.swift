dbf4cca6adf7ef810bcfa90f3c5b08afc38e61ba
Dhipa Poltak <dhipa2poltak@gmail.com>
2023-05-01 17:31:28 +0700

    moving AppError enum to Domain layer

--- TmdbMVVM-UICode/App/AppDelegate.swift
+++ TmdbMVVM-UICode/App/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var coordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         #if DEBUG
             NetworkActivityLogger.shared.level = .debug
             NetworkActivityLogger.shared.startLogging()
